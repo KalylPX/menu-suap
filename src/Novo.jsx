3c013901ab6cf50910a90e2e7f185add79724786
KalylPX <guipaixaogm@gmail.com>
2022-06-09 12:06:25 -0400

    update project

---
+++ src/Novo.jsx
@@ -0,0 +1,39 @@
+function Novo(){
+    return(
+        <div className = "centro">
+
+            <p>NOTÍCIAS PORTAL DO IFRO</p>
+           
+
+            <div className="outro">
+                 <p>NOTA A COMUNIDADE:
+                      Bloqueia de verba lalaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+                  </p>
+
+            
+             </div>
+
+             <div className="outro">
+                  <p>NOTA A COMUNIDADE:
+                      Bloqueia de verba lalaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+                  </p>
+                
+             </div>
+
+             <div className="outro">
+                  <p>NOTA A COMUNIDADE:
+                      Bloqueia de verba lalaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+                  </p>
+                
+             </div>
+
+             <div className="outro">
+                  <p>NOTA A COMUNIDADE:
+                      Bloqueia de verba lalaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+                  </p>
+                
+             </div>
+        </div>
+    )
+}
+export default Novo;
